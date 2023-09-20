904138b3dba6fe7cc4d4771ead4cd2ada21a5f7d
Tyler <m0bley@icloud.com>
2023-09-20 15:34:15 +0100

    Introduced JS functionality, created quiz

---
+++ app.js
@@ -0,0 +1,116 @@
+// Prompting for username and greeting
+
+// !DON'T FORGET TO UNCOMMENT BEFORE PUSH
+// let username = prompt("Hello, welcome to my little corner of the internet. What is your name?");
+
+// while (!username) {
+//   username = prompt("You shall not pass if you don't tell me your name.");
+// }
+
+// alert(`Nice to meet you ${username}, I hope you are having a fantastic day.`);
+
+// Beginning of 5 questions prompt
+
+function quizTime() {
+  let scoreCounter = 0;
+  let choices = "Yes/no (y/n)?";
+
+  function qOneStart() {
+    let qOne = prompt(`I have lived in multiple countries. ${choices}`).toLowerCase();
+
+    if (qOne === "y" || qOne === "yes") {
+      alert("✅ Nice one, you got that one right. +1 to you.");
+      // console.log("✅ Nice one, you got that one right. +1 to you.");
+      scoreCounter++;
+      // console.log(`Current score: ${scoreCounter}`);
+    } else if (qOne === "n" || qOne === "no") {
+      alert("❌ WRONG - I have lived in multiple countries!");
+      // console.log("❌ WRONG - I have lived in multiple countries!");
+      // console.log(`Current score: ${scoreCounter}`);
+    } else if (qOne === null) {
+      alert("🚨 You must write yes or no!");
+      qOneStart();
+    } else {
+      alert("🚨 You must write yes or no!");
+      qOneStart();
+    }
+  }
+  qOneStart();
+
+  function qTwoStart() {
+    let qTwo = prompt(`I have completed University at King's College London. ${choices}`).toLowerCase();
+
+    if (qTwo === "y" || qTwo === "yes") {
+      alert("❌ WRONG - I didn't finish university!");
+      // console.log("❌ WRONG - I didn't finish university!");
+      // console.log(`Current score: ${scoreCounter}`);
+    } else if (qTwo === "n" || qTwo === "no") {
+      alert("✅ Correct. Unfortunately I never got to finish it.");
+      // console.log("✅ Correct. Unfortunately I never got to finish it.");
+      scoreCounter++;
+      // console.log(`Current score: ${scoreCounter}`);
+    }
+  }
+  qTwoStart();
+
+  function qThreeStart() {
+    let qThree = prompt(`I own dogs. ${choices}`).toLowerCase();
+
+    if (qThree === "y" || qThree === "yes") {
+      alert("❌ WRONG - I own birds, not dogs!");
+      // console.log("❌ WRONG - I own birds, not dogs!");
+      // console.log(`Current score: ${scoreCounter}`);
+    } else if (qThree === "n" || qThree === "no") {
+      alert("✅ Correct, I own birds!");
+      // console.log("✅ Correct, I own birds!");
+      scoreCounter++;
+      // console.log(`Current score: ${scoreCounter}`);
+    } else {
+      alert("🚨 You must write yes or no!");
+      qThreeStart();
+    }
+  }
+  qThreeStart();
+
+  function qFourStart() {
+    let qFour = prompt(`My favorite music genres are Rock & Metal. ${choices}`).toLowerCase();
+
+    if (qFour === "y" || qFour === "yes") {
+      alert("✅ Correct, I listen to multiple genres but those two are my favorite.");
+      // console.log("✅ Correct, I listen to multiple genres but those two are my favorite.");
+      scoreCounter++;
+      // console.log(`Current score: ${scoreCounter}`);
+    } else if (qFour === "n" || qFour === "no") {
+      alert("❌ WRONG - They are my favorite genres! I do listen to a variety of music, however.");
+      // console.log("❌ WRONG - They are my favorite genres! I do listen to a variety of music, however.");
+      // console.log(`Current score: ${scoreCounter}`);
+    } else {
+      alert("🚨 You must write yes or no!");
+      qFourStart();
+    }
+  }
+  qFourStart();
+
+  function qFiveStart() {
+    let qFive = prompt(`I self-taught English. ${choices}`).toLowerCase();
+
+    if (qFive === "y" || qFive === "yes") {
+      alert("✅ That's right! I started out with cartoons and music!");
+      // console.log("✅ That's right! I started out with cartoons and music!");
+      scoreCounter++;
+      // console.log(`Current score: ${scoreCounter}`);
+    } else if (qFive === "n" || qFive === "no") {
+      alert("❌ WRONG - I did learn English by myself from cartoons, movies, music, tv shows, etc.");
+      // console.log("❌ WRONG - I did learn English by myself from cartoons, movies, music, tv shows, etc.");
+      // console.log(`Current score: ${scoreCounter}`);
+    } else {
+      alert("🚨 You must write yes or no!");
+      qFiveStart();
+    }
+  }
+  qFiveStart();
+
+  alert(`You have completed my quiz and you have a final score of ${scoreCounter}/5. Nice one!`);
+}
+
+document.getElementById("quiz-btn-start").addEventListener("click", quizTime);
